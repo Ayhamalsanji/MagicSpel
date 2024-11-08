747034347c2cd6e44dbd8c7ad4ee22cd80034d11
Ayhamalsanji <ht24ayals@axevalla.fhsk.se>
2024-11-08 11:56:38 +0100

    Heart

--- Assets/HealthHeartBar.cs
+++ Assets/HealthHeartBar.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerHP : MonoBehaviour
+public class HealthHeartBar : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
---
+++ Assets/HealthHearts.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthHearts : MonoBehaviour
+{
+    public Sprite fullHeart, halfHeart, emptyHeart;
+    Image heartImage;
+
+    // Start is called before the first frame update
+   private void Awake()
+    {
+        heartImage = GetComponent<Image>();
+        
+    }
+    public void SetHeartImage(HeartStatus status)
+    {
+        switch (status)
+        {
+
+            case HeartStatus.Empty:
+                heartImage.sprite = emptyHeart;
+                break;
+            case HeartStatus.Hlaf:
+                heartImage.sprite = halfHeart;
+            case HeartStatus.Full:
+                heartImage.sprite = fullHeart;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+public enum HeartStatus
+{
+
+    Empty = 0,
+    Half = 1,
+    Full = 2,
+
+}
