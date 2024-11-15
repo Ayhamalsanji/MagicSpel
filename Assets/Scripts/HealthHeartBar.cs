4d9fcb3ba278e669aa80e8e8587f719cdb2505d1
Ayhamalsanji <ht24ayals@axevalla.fhsk.se>
2024-11-15 11:30:58 +0100

    Healthbar

--- Assets/Scripts/HealthHeartBar.cs
+++ Assets/Scripts/HealthHeartBar.cs
@@ -54,11 +54,7 @@ public class HealthHeartBar : MonoBehaviour
 
         }
 
-<<<<<<< HEAD
-        hearts = new List<HealthHearts>();
-=======
         hearts = new List<HealthHeart>();
->>>>>>> e4649e1a9b405a86ca040e1bce654b33f119c134
     }
 
     // Start is called before the first frame update
